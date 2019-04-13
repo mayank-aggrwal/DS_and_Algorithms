f68e4025697b10ef555cc52ad221e2306ac6f5e3
ronwon <aggarwal.mayank49@gmail.com>
2019-04-13 20:23:12 +0530

    Added Sieve of Eratosthenes implementation

---
+++ PrimeNumbers/SieveOfEratosthenes_I.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+using namespace std;
+
+void printPrimeTill(int n) {
+    bool *num = new bool[n+1];
+    //SETTING UP ARRAY TO SAVE ALL NUMBERS FROM 1 TO N
+    for(int i=0; i<=n; i++) 
+        num[i] = true;
+    
+    for(int i=2; i<=n; i++) {
+        if(num[i]) {
+            for(int j=2*i; j<=n; j+=i) {
+                num[j] = false;
+            }
+        }
+    }
+    
+    // PRINT THE PRIMES
+    for(int i=2; i<=n; i++) {
+        if(num[i]) 
+            cout << i << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+
+    int n;
+    cin >> n;
+    
+    printPrimeTill(n);
+
+}
