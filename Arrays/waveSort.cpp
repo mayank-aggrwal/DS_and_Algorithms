d208a1bec4a4fb74ba99999d547c8899c5cb7227
ronwon <aggarwal.mayank49@gmail.com>
2019-05-10 23:27:02 +0530

    Added program to arrange array elements in a wave form

---
+++ Arrays/waveSort.cpp
@@ -0,0 +1,48 @@
+// WAVE SORT is not a sorting algorithm
+
+#include <iostream>
+using namespace std;
+
+void printArray(int *arr, int n) {
+
+    cout << "ARRAY: ";
+    for(int i=0; i<n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+}
+
+void waveSort(int *arr, int n) {
+
+    bool dec = true;
+    for(int i=0; i<n-1; i++) {
+
+        if(dec) {
+            if(arr[i]<arr[i+1]) {
+                swap(arr[i], arr[i+1]);
+            }
+        }
+        else {
+            if(arr[i]>arr[i+1]) {
+                swap(arr[i], arr[i+1]);
+            }
+        }
+        dec = !dec;
+
+    }
+
+}
+
+int main() {
+
+    int arr[] = {1,2,3,0,6,88,89,43,21};
+    int n = sizeof(arr)/sizeof(int);
+    printArray(arr, n);
+
+    waveSort(arr, n);
+
+    cout << "After wave sort - ";
+    printArray(arr, n);
+
+}
