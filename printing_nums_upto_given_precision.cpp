c8ed303945c1d915644c7ea334788063b120d94b
mayank-aggrwal <aggarwal.mayank49@gmail.com>
2021-02-22 04:40:41 +0530

    Printing numbers upto given precision, setprecision, fixed

---
+++ printing_nums_upto_given_precision.cpp
@@ -0,0 +1,12 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+
+    double a = 5.62375212;
+    cout << a << endl;
+    cout << setprecision(3) << a << endl;
+    cout << fixed << setprecision(3) << a << endl;
+    
+}
